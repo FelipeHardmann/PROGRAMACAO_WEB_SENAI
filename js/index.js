0b3d55f02e4e27ba6595f82181cee6a787f01ca9
Felipe Hardmann <fashardmann@gmail.com>
2022-03-18 21:33:37 -0300

    novo

--- js/index.js
+++ js/index.js
@@ -2,23 +2,50 @@
 var appEl = document.querySelector('#app');
 
 //Criar um elemento
-var senhaEl = document.createElement('input');
+/*var senhaEl = document.createElement('input');
 senhaEl.setAttribute('type', 'password');
 
 //atrelar o senhaEl ao appEl
-appEl.appendChild(senhaEl);
+appEl.appendChild(senhaEl);*/
+
+
+var btn = document.createElement('button');//Botão
+btn.setAttribute('type','button');
+var textobotao = document.createTextNode("Crie seu quadrado");
+btn.appendChild(textobotao)
+appEl.appendChild(btn);
+//Texto para escrever
+var textoquadrado = document.createElement('input');
+textoquadrado.setAttribute('type', 'input');
+appEl.appendChild(textoquadrado);
+//Função para gerar os quadrados
+
+btn.onclick = function(){
+    var quadradoM = document.createElement('div');
+    quadradoM.style.width = '100px';
+    quadradoM.style.height = '100px';
+    quadradoM.style.backgroundColor = textoquadrado.value;
+    quadradoM.style.border = '2px solid black';
+    appEl.appendChild(quadradoM);
+}
 
 
 
 
-//Quadrado1 - criar elemento
+
+
+
+
+
+
+
+/*//Quadrado1 - criar elemento
 var quadrado1 = document.createElement('div');
 
 //Criando quadrado
 quadrado1.style.height = '100px';
 quadrado1.style.width = '100px';
 quadrado1.style.backgroundColor = 'red';
-
 appEl.appendChild(quadrado1);
 
 //Circulo
@@ -34,7 +61,16 @@ var retangulo = document.createElement('div');
 retangulo.style.height = '100px';
 retangulo.style.width = '300px';
 retangulo.style.backgroundColor = 'green';
-appEl.appendChild(retangulo);   
+appEl.appendChild(retangulo); */
+
+//Quadrado mágico
+/*var texto = document.createElement('input');
+texto.setAttribute();
+appEl.appendChild(texto);*/
+
+
+
+
 
 
 
